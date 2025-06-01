24a650d5dd062fb0693fc5f74fbd0ee8a18eb6e0
Manav <patelmanav2406@gmail.com>
2025-06-02 01:39:10 +0530

    layout update

---
+++ server/routes/layout.route.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { authorizedRoles, isAuthenticated } from "../middleware/auth";
+import { createLayout, editLayout, getLayoutByType } from "../controllers/layout.controller";
+const layoutRouter = express.Router();
+
+layoutRouter.post("/create-layout", isAuthenticated, authorizedRoles("admin"), createLayout);
+layoutRouter.put("/edit-layout", isAuthenticated, authorizedRoles("admin"), editLayout);
+layoutRouter.get("/get-layout", isAuthenticated, getLayoutByType);
+export default layoutRouter;
