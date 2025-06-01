f69dff938faf77151fd5fe8b488d3d82b14fa072
Manav <patelmanav2406@gmail.com>
2025-06-02 00:10:51 +0530

    analytics update

---
+++ server/routes/analytics.route.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { authorizedRoles, isAuthenticated } from '../middleware/auth';
+import { getCourseAnalytics, getOrderAnalytics, getUserAnalytics } from '../controllers/analytics.controller';
+const analyticsRouter = express.Router();
+
+analyticsRouter.get("/get-user-analytics", isAuthenticated, authorizedRoles("admin"), getUserAnalytics);
+analyticsRouter.get("/get-course-analytics", isAuthenticated, authorizedRoles("admin"), getCourseAnalytics);
+analyticsRouter.get("/get-order-analytics", isAuthenticated, authorizedRoles("admin"), getOrderAnalytics);
+
+export default analyticsRouter;
