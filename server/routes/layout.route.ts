eb6c62fb29198c626899292bc368a0e1f22627d4
Manav <patelmanav2406@gmail.com>
2025-06-06 01:24:02 +0530

    updates

--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -6,4 +6,5 @@ const layoutRouter = express.Router();
 layoutRouter.post("/create-layout", isAuthenticated, authorizedRoles("admin"), createLayout);
 layoutRouter.put("/edit-layout", isAuthenticated, authorizedRoles("admin"), editLayout);
 layoutRouter.get("/get-layout", isAuthenticated, getLayoutByType);
+
 export default layoutRouter;
