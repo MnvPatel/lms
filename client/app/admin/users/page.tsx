5fe5ed8125024d343c60aa55ae114049a536ba97
Manav <patelmanav2406@gmail.com>
2025-07-13 23:52:39 +0530

    get all users

---
+++ client/app/admin/users/page.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import AdminProtected from '@/app/hooks/adminProtected';
+import Heading from '@/app/utils/Heading';
+import AdminSideBar from '../../components/Admin/sidebar/AdminSidebar'
+import React from 'react'
+import DashboardHero from '@/app/components/Admin/DashboardHero';
+import AllCourses from '@/app/components/Admin/Course/AllCourses';
+import AllUsers from '@/app/components/Admin/Course/AllUsers';
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <AdminProtected>
+        <Heading
+          title="ELearning - Admin"
+          description="ELearning is a platform for students to learn and get help from teachers"
+          keywords="Programming, MERN, ML"
+        />
+        <div className="flex h-screen">
+          <div className="1500px:w-[16%] w-1/5">
+            <AdminSideBar />
+          </div>
+          <div className="w-[85%]">
+            <DashboardHero/>
+            <AllUsers />
+          </div>
+        </div>
+      </AdminProtected>
+    </div>
+  );
+}
+
+export default page
